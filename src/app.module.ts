d5bc7808a8e8f0a15e76328718e598f8475faf44
rifky <krifky14@yahoo.co.id>
2025-06-29 10:55:14 +0700

    init: nest and role

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
+
+@Module({
+  imports: [UserModule, AccountModule, TransactionModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
