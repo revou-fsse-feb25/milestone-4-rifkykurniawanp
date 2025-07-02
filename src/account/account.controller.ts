fa192e452b080f4afdf9fe1ccf620490908e3d72
rifky <krifky14@yahoo.co.id>
2025-07-02 17:22:04 +0700

    feat: implement account retrieval and data structure

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { accounts } from './account.data';
+import { NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+    @Get()
+    getAllAccounts() {
+        return accounts;
+    }
+    @Get(':id')
+getAccountById(@Param('id', ParseIntPipe) id: number) {
+  const account = accounts.find(acc => acc.id === id);
+  if (!account) {
+    throw new NotFoundException(`Account with ID ${id} not found`);
+  }
+  return account;
+}
+
+    }
