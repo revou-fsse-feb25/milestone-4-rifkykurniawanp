b4832aed2944d7f6fa39ead25a774db809010ed0
rifky <krifky14@yahoo.co.id>
2025-07-03 18:46:16 +0700

    feat: implement account update and delete functionality in AccountService and AccountController; add user data structure

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Body, Delete } from '@nestjs/common';
 import { accounts } from './account.data';
 import { NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
-import { CreateProductDto } from './DTO/create-product.dto';
+import { CreateProductDto} from './DTO/create-product.dto';
+import { UpdateProductDto } from './DTO/update-product.dto';
+import { AccountService } from './account.service';
 
 @Controller('account')
 export class AccountController {
+  constructor(private readonly accountService: AccountService) {}
+
   @Get()
   getAllAccounts() {
     return accounts;
@@ -25,4 +29,17 @@ export class AccountController {
       data: CreateProductDto,
     };
   }
+
+  @Patch(':id')
+updateAccount(
+  @Param('id', ParseIntPipe) id: number,
+  @Body() updateProductDto: UpdateProductDto,
+) {
+  return this.accountService.updateAccount(id, updateProductDto);
+}
+
+@Delete(':id')
+deleteAccount(@Param('id', ParseIntPipe) id: number) {
+  return this.accountService.deleteAccount(id);
+}
 }
--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,1 +1,11 @@
-export interface User {}
+export interface User {
+  id: number;
+  fullName: string;
+  email: string;
+  phone: string;
+  address: string;
+  accountNumber: string;
+  balance: number;
+  createdAt: Date;
+  isActive: boolean;
+}
